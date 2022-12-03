dd453d5f9d5329b4e78dd667485dee30460c634d
franzistutt <116589626+franzistutt@users.noreply.github.com>
2022-12-03 14:56:00 +0100

    Update webapp_2.py

--- webapps/webapp_2.py
+++ webapps/webapp_2.py
@@ -1,81 +1,268 @@
-from js import window, document, console
-from js import THREE, Math
-from pyodide import create_proxy, to_js
+# Import javascript modules
+from js import THREE, window, document, Object, console
+# Import pyscript / pyodide modules
+from pyodide.ffi import create_proxy, to_js
+# Import python module
+import math
+
+# ----------------------------------------------------------------------------------------------------------
+global tree_params    
+tree_params = {  
+        "size" : 1,
+        "old_size" : 2,
+        "height" :4,
+        "old_height" : 1,
+        "x" : 4,
+        "y" : 2,
+        "old_x" : 2,
+        "old_y" : 1,
+}
+tree_params = Object.fromEntries(to_js(tree_params))
 
 
+#-----------------------------------------------------------------------
+#main
 def main():
-    global objects, control_points, transform_control, action_select
-    objects = []
-    control_points = []
+    #-----------------------------------------------------------------------
+    # VISUAL SETUP
+    # Declare the variables
+    global renderer, scene, camera, controls,composer, texture
 
-    global camera
-    camera = THREE.PerspectiveCamera.new( 45, window.innerWidth / window.innerHeight, 1, 10000 )
-    camera.position.set( 500, 800, 1300 )
-    camera.lookAt( 0, 0, 0 )
-
-    global scene
-    scene = THREE.Scene.new()
-    scene.background = THREE.Color.new( "rgb(40,40,40)" )
-
-    #roll-over helpers
-    roll_over_geom = THREE.CircleGeometry.new( 10, 20 )
-    roll_over_material = THREE.MeshBasicMaterial.new()
-    roll_over_material.color = THREE.Color.new(0,255,0)
-    roll_over_material.opacity = 0.5
-    roll_over_material.transparent = True
-    
-    global roll_over_mesh
-    roll_over_mesh = THREE.Mesh.new( roll_over_geom, roll_over_material )
-    roll_over_mesh.rotation.x = Math.PI * -0.5
-    scene.add( roll_over_mesh )
-
-    #spheres
-    global sphere_geom, sphere_material
-    sphere_geom = THREE.SphereGeometry.new( 10, 20, 20 )
-    sphere_material = THREE.MeshBasicMaterial.new()
-    sphere_material.color = THREE.Color.new( "rgb(255,255,255" )
-
-	#grid
-    grid_helper = THREE.GridHelper.new( 1000, 20 )
-    grid_helper.position.y = -1
-    scene.add( grid_helper )
-
-    #raycaster
-    global raycaster
-    raycaster = THREE.Raycaster.new()
-    global pointer
-    pointer = THREE.Vector2.new()
-
-    geometry = THREE.PlaneGeometry.new( 1000, 1000 )
-    geometry.rotateX( - Math.PI / 2 )
-
-    global plane
-    plane = THREE.Mesh.new( geometry, THREE.MeshBasicMaterial.new())
-    plane.visible = False
-    scene.add( plane )
-
-    #lights
-    ambientLight = THREE.AmbientLight.new( 0x606060 )
-    scene.add( ambientLight )
-
-    directionalLight = THREE.DirectionalLight.new( 0xffffff )
-    directionalLight.position.set( 1, 0.75, 0.5 ).normalize()
-    scene.add( directionalLight )
-
-    #renderer
-    global renderer
+    #Set up the renderer
     renderer = THREE.WebGLRenderer.new()
     renderer.setPixelRatio( window.devicePixelRatio )
-    renderer.setSize( window.innerWidth, window.innerHeight )
-    document.body.appendChild(renderer.domElement )
+    renderer.setSize(window.innerWidth, window.innerHeight)
 
-    #transform_control
-    transform_control = THREE.TransformControls.new(camera, renderer.domElement)
-    transform_drag_proxy = create_proxy(transform_drag)
-    transform_control.addEventListener('dragging-changed', transform_drag_proxy)
+    document.body.appendChild(renderer.domElement)
     
-    #post processing
-    global render_pass, fxaa_pass
+    # texture = THREE.TextureLoader.new().load("green2.jpg")
+    
+    # Set up the scene
+    scene = THREE.Scene.new()
+    back_color = THREE.Color.new("rgb(255,255,255)")
+    scene.background = back_color
+    
+    camera = THREE.PerspectiveCamera.new(75, window.innerWidth/window.innerHeight, 0.1, 10000)
+    camera.position.z = 50
+
+    scene.add(camera)
+
+    
+    # Graphic Post Processing
+    global composer
+    post_process()
+
+
+    # Set up responsive window
+    resize_proxy = create_proxy(on_window_resize)
+    window.addEventListener('resize', resize_proxy) 
+     #-----------------------------------------------------------------------
+
+    # Mouse orbit
+    controls = THREE.OrbitControls.new(camera, renderer.domElement)
+    
+   
+    #GUI
+    gui = window.dat.GUI.new()
+    sizechanger = gui.addFolder('properties')
+    sizechanger.add(tree_params, 'size')
+    sizechanger.add(tree_params, 'height')
+   
+    sizechanger.open()
+
+    param_folder = gui.addFolder('field')
+    param_folder.add(tree_params, 'x', 2,20,1)
+    param_folder.add(tree_params, 'y', 2,20,1)
+
+    param_folder.open()
+    #-----------------------------------------------------------------------
+    # Geometry
+    
+    my_axiom_system = system(0, tree_params.size, "X")
+    console.log(my_axiom_system)
+    
+    global color  
+    color = THREE.Color.new('rgb(120,100,80)')
+        
+    draw_system(my_axiom_system, THREE.Vector3.new(0,0,0),color,15,math.pi/3, math.pi/3)
+
+    
+    
+    #-----------------------------------------------------------------------
+    # render update, 
+    render()
+
+# ----------------------------------------------------------------------------------------------------------
+
+def update_landscape():
+    if tree_params.old_x != tree_params.x or tree_params.old_y != tree_params.y or tree_params.old_size != tree_params.size or tree_params.old_height != tree_params.height:
+        scene.clear()
+                     
+        for i in range(tree_params.x):
+            for j in range(tree_params.y):
+            
+                my_axiom_system = system(0,tree_params.size, "X")
+                
+                draw_system(my_axiom_system, THREE.Vector3.new(i * 40,0,j*40),color,tree_params.height,math.pi/(3+(10/((i*0.5)*(j*0.5)+0.5))),math.pi/(3+(5/((j*0.5)*(i*0.5)+0.5))))
+       
+        tree_params.old_height = tree_params.height
+        tree_params.old_size = tree_params.size
+        tree_params.old_x = tree_params.x
+        tree_params.old_y = tree_params.y
+                 
+        
+ 
+        
+          
+#-----------------------------------------------------------------------
+# rule 
+def generate(symbol):
+
+    if symbol == "X":
+        return "[FX[+X][-X][*X][/X]X]"
+    elif symbol == "F":
+        return "FF"
+    elif symbol == "+":
+        return "+"
+    elif symbol == "-":
+        return "-"
+    elif symbol == "[":
+        return "["
+    elif symbol == "]":
+        return "]"
+    elif symbol == "/":
+        return "/"
+    elif symbol == "*":
+        return "*"
+    
+   
+# ----------------------------------------------------------------------------------------------------------
+# recursive function
+def system(current_iteration, max_iterations, axiom):
+    current_iteration += 1
+    new_axiom = ""
+    for symbol in axiom:
+        new_axiom += generate(symbol)
+    
+    # print(current_iteration)
+    # print(new_axiom)
+ 
+
+    if current_iteration >= max_iterations:
+        return new_axiom
+    else:
+        
+        return system(current_iteration, max_iterations, new_axiom)
+
+
+# ----------------------------------------------------------------------------------------------------------
+
+def draw_system(axiom, start_pt,color,height, angle_z, angle_x):
+    move_vec = THREE.Vector3.new(0,height,0)
+    old_states = []
+    old_move_vecs = []
+    lines = []
+    apples = []
+    
+    for symbol in axiom:
+        if symbol == "F" or symbol == "X":
+            old = THREE.Vector3.new(start_pt.x, start_pt.y, start_pt.z)
+            new_pt = THREE.Vector3.new(start_pt.x, start_pt.y, start_pt.z)
+            new_pt = new_pt.add(move_vec)
+            
+            line = []
+            line.append(old)
+            line.append(new_pt)
+            lines.append(line)
+
+            start_pt = new_pt
+            
+            if old.x != new_pt.x or old.z != new_pt.z:
+                apples.append(new_pt)
+        
+        
+             
+        elif symbol == "+": 
+            move_vec.applyAxisAngle(THREE.Vector3.new(0,0,1), angle_z)
+        
+        elif symbol == "-": 
+            move_vec.applyAxisAngle(THREE.Vector3.new(0,0,1), -angle_z)
+            
+        elif symbol == "/": 
+            move_vec.applyAxisAngle(THREE.Vector3.new(1,0,0), angle_x)
+        
+        elif symbol == "*":
+            move_vec.applyAxisAngle(THREE.Vector3.new(1,0,0), -angle_x)
+        
+        elif symbol == "[":
+            old_state = THREE.Vector3.new(start_pt.x, start_pt.y, start_pt.z)
+            old_move_vec = THREE.Vector3.new(move_vec.x, move_vec.y, move_vec.z)
+            old_states.append(old_state)
+            old_move_vecs.append(old_move_vec)              
+
+        elif symbol == "]":
+            start_pt = THREE.Vector3.new(old_states[-1].x, old_states[-1].y, old_states[-1].z)
+            move_vec = THREE.Vector3.new(old_move_vecs[-1].x, old_move_vecs[-1].y, old_move_vecs[-1].z)
+            old_states.pop(-1)
+            old_move_vecs.pop(-1)
+            
+      
+    
+    for points in lines:
+        
+        line_geom = THREE.BufferGeometry.new()
+        points = to_js(points)
+        
+        console.log(points)
+
+        line_geom.setFromPoints( points )
+        
+
+        line_material = THREE.LineBasicMaterial.new(color)
+        line_material.color = color
+        
+        vis_line = THREE.Line.new(line_geom, line_material)
+        
+        scene.add(vis_line)
+        
+        
+    for points in apples:
+        # print(points.x)
+        
+        apple = THREE.SphereGeometry.new(0.5,15, 15)
+        apple.translate(points.x, points.y - 0.5, points.z)
+    
+        points = to_js(points)
+        console.log(points)
+    
+        
+        color_apple = THREE.Color.new(255, 0, 0)
+        material = THREE.MeshBasicMaterial.new(color_apple)
+        material.transparent = True
+        material.opacity = 0.2
+        material.color = color  
+        material.color = color_apple
+        
+        spheres = THREE.Mesh.new(apple, material)
+        
+        scene.add(spheres)
+                
+        
+    
+# ----------------------------------------------------------------------------------------------------------
+
+# Simple render and animate
+def render(*args):
+    window.requestAnimationFrame(create_proxy(render))
+    update_landscape()
+
+    controls.update()
+    composer.render()
+    
+    
+
+# Graphical post-processing
+def post_process():
     render_pass = THREE.RenderPass.new(scene, camera)
     render_pass.clearColor = THREE.Color.new(0,0,0)
     render_pass.ClearAlpha = 0
@@ -85,108 +272,13 @@ def main():
 
     fxaa_pass.material.uniforms.resolution.value.x = 1 / ( window.innerWidth * pixelRatio )
     fxaa_pass.material.uniforms.resolution.value.y = 1 / ( window.innerHeight * pixelRatio )
-
+   
     global composer
     composer = THREE.EffectComposer.new(renderer)
     composer.addPass(render_pass)
     composer.addPass(fxaa_pass)
-    
 
-    #orbit controls
-    controls = THREE.OrbitControls.new(camera, renderer.domElement)
-    controls.update()
-    
-    resize_proxy = create_proxy(on_window_resize)
-    window.addEventListener('resize', resize_proxy)
-
-    pointer_move_proxy = create_proxy(on_pointer_move)
-    document.addEventListener('pointermove', pointer_move_proxy)
-
-    dbl_click_proxy = create_proxy(on_dbl_click)
-    document.addEventListener('dblclick', dbl_click_proxy)
-    
-    #initiate empty curve
-    global curve_object
-    curve_object = None
-
-    global extruded_curves
-    extruded_curves = []
-
-    render()
-
-
-def transform_drag(event):
-    event.preventDefault()
-
-    if not event.value:
-        update_curve()
-        extrude_curve()
-    
-
-def update_curve():
-    #curve
-    #initiate the first segment
-    global _range, curve_object
-    _range = len(control_points)
-
-    if _range == 2 and curve_object == None:
-        js_control_points = to_js(control_points)
-        geometry = THREE.BufferGeometry.new()
-        geometry.setFromPoints( js_control_points )
-
-        curve_material = THREE.LineBasicMaterial.new()
-        curve_material.color = THREE.Color.new("rgb(255,255,255")
-
-        curve_object = THREE.LineLoop.new( geometry, curve_material )
-        scene.add(curve_object)
-
-    elif _range>2:
-        scene.remove(curve_object)
-
-        js_control_points = to_js(control_points)
-        geometry = THREE.BufferGeometry.new()
-        geometry.setFromPoints( js_control_points )
-
-        curve_material = THREE.LineBasicMaterial.new()
-        curve_material.color = THREE.Color.new("rgb(255,255,255")
-
-        curve_object = THREE.LineLoop.new( geometry, curve_material )
-
-        scene.add(curve_object)
-    
-    global extruded_curves
-    if len(extruded_curves) == 100:
-        for curve in extruded_curves:
-                scene.remove(curve)
-                extruded_curves = []
-
-def extrude_curve():
-    global extruded_curves, curve_object
-    if curve_object != None:
-        if len(extruded_curves) < 100:
-            pass
-            for i in range(100):
-                extrude_curve_mat = THREE.LineBasicMaterial.new()
-
-                extrude_curve_mat.color = THREE.Color.new(0.5, 0.5, (i+1)/100)
-
-
-
-                extrude_curve = THREE.LineLoop.new()
-                extrude_curve.geometry.copy(curve_object.geometry)
-                extrude_curve.position.y = (i+1)*20
-
-                extrude_curve.material = extrude_curve_mat
-            
-                extruded_curves.append(extrude_curve)
-                scene.add(extrude_curve)   
-        
-def render(*args):
-    window.requestAnimationFrame(create_proxy(render))
-
-    global composer
-    composer.render()
-
+# window size
 def on_window_resize(event):
 
     event.preventDefault()
@@ -199,69 +291,9 @@ def on_window_resize(event):
 
     renderer.setSize( window.innerWidth, window.innerHeight )
 
-    #post processing
-    global render_pass, fxaa_pass
-    render_pass = THREE.RenderPass.new(scene, camera)
-    render_pass.clearColor = THREE.Color.new(0,0,0)
-    render_pass.ClearAlpha = 0
-    fxaa_pass = THREE.ShaderPass.new(THREE.FXAAShader)
-
-    pixelRatio = window.devicePixelRatio
-
-    fxaa_pass.material.uniforms.resolution.value.x = 1 / ( window.innerWidth * pixelRatio )
-    fxaa_pass.material.uniforms.resolution.value.y = 1 / ( window.innerHeight * pixelRatio )
-
-    global composer
-    composer = THREE.EffectComposer.new(renderer)
-    composer.addPass(render_pass)
-    composer.addPass(fxaa_pass)
-    
-
-def on_pointer_move(event):
-
-    event.preventDefault()
-
-    global raycaster, pointer, objects, roll_over_mesh
-
-    pointer.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 )
-
-    raycaster.setFromCamera( pointer, camera )
-
-    intersects = raycaster.intersectObjects( plane, True )
-
-    if intersects.length > 0 :
-        intersect = intersects[ 0 ]
-        
-        roll_over_mesh.position.copy( intersect.point ).add( intersect.face.normal )
-
-def on_drag (event):
-    pass     
-
-def on_dbl_click(event):
-    event.preventDefault()
-
-    global raycaster, pointer, objects, sphere_geom, sphere_material, control_points
-
-    pointer.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 )
-
-    raycaster.setFromCamera( pointer, camera )
-
-    intersects = raycaster.intersectObject( plane, True )
-
-    if intersects.length > 0 :
-        intersect = intersects[ 0 ]
-        point = THREE.Mesh.new( sphere_geom, sphere_material )
-        point.rotation.x = Math.PI * -0.5
-        point.position.copy( intersect.point ).add( intersect.face.normal )
-
-        control_points.append(point.position)
-        scene.add(point)
-
-        objects.append(point)
-
-    update_curve()
-    extrude_curve()
-
-
-if __name__ == '__main__':
-        main()
+    #post processing after resize
+    post_process()
+#-----------------------------------------------------------------------
+#RUN THE MAIN PROGRAM
+if __name__=='__main__':
+    main()
