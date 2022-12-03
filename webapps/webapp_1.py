cb72089c2869aca1992e6c585443ded91fa4f69c
franzistutt <116589626+franzistutt@users.noreply.github.com>
2022-12-03 14:54:35 +0100

    Update webapp_1.py

--- webapps/webapp_1.py
+++ webapps/webapp_1.py
@@ -1,75 +1,194 @@
-from js import window, document, console
-from js import THREE, Math
-from pyodide import create_proxy, to_js
+# Import javascript modules
+from js import THREE, window, document, Object
+# Import pyscript / pyodide modules
+from pyodide.ffi import create_proxy, to_js
+# Import python module
+import math
 
+# ----------------------------------------------------------------------------------------------------------
 
 def main():
-    global objects
-    objects = []
+    #-----------------------------------------------------------------------
 
-    global camera
-    camera = THREE.PerspectiveCamera.new( 45, window.innerWidth / window.innerHeight, 1, 10000 )
-    camera.position.set( 500, 800, 1300 )
-    camera.lookAt( 0, 0, 0 )
-
-    global scene
-    scene = THREE.Scene.new()
-    scene.background = THREE.Color.new( 0xf0f0f0 )
-
-    #roll-over helpers
-    rollOverGeo = THREE.BoxGeometry.new( 50, 50, 50 )
-    rollOverMaterial = THREE.MeshBasicMaterial.new()
-    rollOverMaterial.color = THREE.Color.new( 0xff0000 )
-    rollOverMaterial.opacity = 0.5
-    rollOverMaterial.transparent = True
-
-    global rollOverMesh
-    rollOverMesh = THREE.Mesh.new( rollOverGeo, rollOverMaterial )
-    scene.add( rollOverMesh )
-
-    #cubes
-    global cubeGeo, cubeMaterial
-    cubeGeo = THREE.BoxGeometry.new( 50, 50, 50 )
-    cubeMaterial = THREE.MeshNormalMaterial.new()
-    #cubeMaterial.color = THREE.Color.new( 0xfeb74c )
-
-	#grid
-    gridHelper = THREE.GridHelper.new( 1000, 20 )
-    scene.add( gridHelper )
-
-    #raycaster
-    global raycaster
-    raycaster = THREE.Raycaster.new()
-    global pointer
-    pointer = THREE.Vector2.new()
-
-    geometry = THREE.PlaneGeometry.new( 1000, 1000 )
-    geometry.rotateX( - Math.PI / 2 )
-
-    global plane
-    plane = THREE.Mesh.new( geometry, THREE.MeshBasicMaterial.new())
-    plane.visible = False
-    scene.add( plane )
-
-    objects.append( plane)
-
-
-    #lights
-    ambientLight = THREE.AmbientLight.new( 0x606060 )
-    scene.add( ambientLight )
-
-    directionalLight = THREE.DirectionalLight.new( 0xffffff )
-    directionalLight.position.set( 1, 0.75, 0.5 ).normalize()
-    scene.add( directionalLight )
-
-    global renderer
+    global renderer, scene, camera, controls,composer, axesHelper
+    
+    #renderer
     renderer = THREE.WebGLRenderer.new()
     renderer.setPixelRatio( window.devicePixelRatio )
-    renderer.setSize( window.innerWidth, window.innerHeight )
-    document.body.appendChild(renderer.domElement )
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
 
-    #post processing
-    global render_pass, fxaa_pass
+    #scene
+    scene = THREE.Scene.new()
+    back_color = THREE.Color.new(0.1,0.1,0.1)
+    scene.background = back_color
+    camera = THREE.PerspectiveCamera.new(75, window.innerWidth/window.innerHeight, 0.1, 1000)
+    camera.position.z = 50
+    scene.add(camera)
+
+    # Graphic Post Processing
+    global composer
+    post_process()
+
+    #axesHelper
+    axesHelper = THREE.AxesHelper.new(100)
+    scene.add(axesHelper)
+    
+    #responsive window
+    resize_proxy = create_proxy(on_window_resize)
+    window.addEventListener('resize', resize_proxy) 
+   
+    #-----------------------------------------------------------------------
+    # Geometry 
+    global geom1_params, cubes, cube_lines
+    
+    cubes = []
+    cube_lines = []
+    
+    
+    geom1_params = {
+        "size" : 2,
+        "scale": 0.5,
+        "x" : 2, 
+        "y" : 2,
+        "z" : 1,
+        "rotation":0,
+        "rotation2":0,
+
+    }
+    
+    geom1_params = Object.fromEntries(to_js(geom1_params))
+    
+    
+    #create Materials
+    global material, line_material
+    
+    color = THREE.Color.new(255, 255, 255)
+    material = THREE.MeshBasicMaterial.new(color)
+    material.transparent = True
+    material.opacity = 0.5
+    material.color = color
+    
+    line_material = THREE.LineBasicMaterial.new()
+    line_material.color = THREE.Color.new(255,255,255)
+    
+    Matrix()
+    
+    
+    #-----------------------------------------------------------------------
+    # Mouse orbit control
+    controls = THREE.OrbitControls.new(camera, renderer.domElement)
+
+    # GUI
+    gui = window.dat.GUI.new()
+    param_folder = gui.addFolder('Parameters')
+    param_folder.add(geom1_params, 'size', 0.5,10,0.1)
+    param_folder.add(geom1_params, 'scale', 0.5,1,0.1)
+    param_folder.add(geom1_params, 'x', 1,15,1)
+    param_folder.add(geom1_params, 'y', 1,30,1)
+    param_folder.add(geom1_params, 'z', 1,20,1)
+    param_folder.add(geom1_params, 'rotation', 0,180)
+    param_folder.add(geom1_params, 'rotation2', 0,180)
+  
+    param_folder.open()
+    
+    #-----------------------------------------------------------------------
+    render()
+    
+# ----------------------------------------------------------------------------------------------------------
+
+
+                    
+def Matrix():
+        for k in range(geom1_params.y):
+            for i in range(geom1_params.x):
+                for j in range(geom1_params.z):
+                
+                    #for update
+                    global z,sizeold, scaleold, rotateold
+                    z = geom1_params.rotation2
+                    sizeold = geom1_params.size
+                    scaleold = geom1_params.scale
+                    rotateold = geom1_params.rotation
+                    
+                    #generate geometry
+                    geom = THREE.BoxGeometry.new(geom1_params.size*geom1_params.scale*(i+1), geom1_params.size, geom1_params.size*geom1_params.scale*(j+1))
+                    
+                    geom.translate((geom1_params.size*i)*(geom1_params.size+i), geom1_params.size*k, (geom1_params.size*j)*(geom1_params.size+j))
+                    
+                    cube = THREE.Mesh.new(geom, material)
+                    
+                    #rotating around center
+                    bbox = THREE.Box3.new()
+                    bbox.setFromObject(cube)
+                    center = THREE.Vector3.new()
+                    bbox.getCenter(center)
+                    center.normalize()
+                    cube.rotateOnAxis(center, math.radians(geom1_params.rotation))
+    
+                    #rotating around axis
+                    geom.rotateY(math.radians(geom1_params.rotation2)/geom1_params.y*k)
+                
+                     
+                    cubes.append(cube)
+                    
+                    #generate line
+                    edges = THREE.EdgesGeometry.new(cube.geometry)
+                    line = THREE.LineSegments.new(edges, line_material)
+                    line.rotateOnAxis(center, math.radians(geom1_params.rotation))
+                    
+                    cube_lines.append(line)
+                    scene.add(line,cube)
+                
+                
+
+# ----------------------------------------------------------------------------------------------------------
+               
+def update_cubes():
+    global cubes, cube_lines, material, line_material
+    # Make sure you dont have zero cubes 
+    
+    if len(cubes) != 0 :
+        if len(cubes) != geom1_params.x * geom1_params.y * geom1_params.z:
+            for cube in cubes: scene.remove(cube)
+            for cube in cube_lines: scene.remove(cube)
+            
+            cube = []
+            cube_lines = []
+            
+            Matrix()
+    
+        
+    if z != geom1_params.rotation2 or sizeold != geom1_params.size or scaleold != geom1_params.scale or rotateold != geom1_params.rotation:
+        for cube in cubes: scene.remove(cube)
+        for cube in cube_lines: scene.remove(cube)
+            
+        cube = []
+        cube_lines = []
+           
+        Matrix()
+    
+ 
+        
+        
+            
+        
+            
+
+
+                
+
+# ----------------------------------------------------------------------------------------------------------
+               
+# Simple render and animate
+def render(*args):
+    window.requestAnimationFrame(create_proxy(render))
+    update_cubes()
+    controls.update()
+    composer.render()
+
+# Graphical post-processing
+def post_process():
     render_pass = THREE.RenderPass.new(scene, camera)
     render_pass.clearColor = THREE.Color.new(0,0,0)
     render_pass.ClearAlpha = 0
@@ -79,35 +198,13 @@ def main():
 
     fxaa_pass.material.uniforms.resolution.value.x = 1 / ( window.innerWidth * pixelRatio )
     fxaa_pass.material.uniforms.resolution.value.y = 1 / ( window.innerHeight * pixelRatio )
-
+   
     global composer
     composer = THREE.EffectComposer.new(renderer)
     composer.addPass(render_pass)
     composer.addPass(fxaa_pass)
-    
 
-    #orbit controls
-    controls = THREE.OrbitControls.new(camera, renderer.domElement)
-    controls.update()
-    
-    resize_proxy = create_proxy(on_window_resize)
-    window.addEventListener('resize', resize_proxy)
-
-    pointer_move_proxy = create_proxy(on_pointer_move)
-    document.addEventListener('pointermove', pointer_move_proxy)
-
-    dbl_click_proxy = create_proxy(on_dbl_click)
-    document.addEventListener('dblclick', dbl_click_proxy)
-    
-
-    render()
-
-
-def render(*args):
-    global composer1, composer2
-    window.requestAnimationFrame(create_proxy(render))
-    composer.render()
-
+# Adjust display when window size changes
 def on_window_resize(event):
 
     event.preventDefault()
@@ -120,67 +217,9 @@ def on_window_resize(event):
 
     renderer.setSize( window.innerWidth, window.innerHeight )
 
-    #post processing
-    global render_pass, fxaa_pass
-    render_pass = THREE.RenderPass.new(scene, camera)
-    render_pass.clearColor = THREE.Color.new(0,0,0)
-    render_pass.ClearAlpha = 0
-    fxaa_pass = THREE.ShaderPass.new(THREE.FXAAShader)
-
-    pixelRatio = window.devicePixelRatio
-
-    fxaa_pass.material.uniforms.resolution.value.x = 1 / ( window.innerWidth * pixelRatio )
-    fxaa_pass.material.uniforms.resolution.value.y = 1 / ( window.innerHeight * pixelRatio )
-
-    global composer
-    composer = THREE.EffectComposer.new(renderer)
-    composer.addPass(render_pass)
-    composer.addPass(fxaa_pass)
-    
-
-def on_pointer_move(event):
-
-    event.preventDefault()
-
-    global raycaster, pointer, objects
-
-    pointer.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 )
-
-    raycaster.setFromCamera( pointer, camera )
-
-    js_objects = to_js(objects)
-    intersects = raycaster.intersectObjects( js_objects, True )
-
-    if intersects.length > 0 :
-        intersect = intersects[ 0 ]
-        
-        rollOverMesh.position.copy( intersect.point ).add( intersect.face.normal )
-        rollOverMesh.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25 )
-
-def on_dbl_click(event):
-    event.preventDefault()
-
-    global raycaster, pointer, objects, cubeGeo, cubeMaterial
-
-    pointer.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 )
-
-    raycaster.setFromCamera( pointer, camera )
-
-    js_objects = to_js(objects)
-    intersects = raycaster.intersectObjects( js_objects, True )
-
-    if intersects.length > 0 :
-        intersect = intersects[ 0 ]
-        voxel = THREE.Mesh.new( cubeGeo, cubeMaterial )
-        voxel.position.copy( intersect.point ).add( intersect.face.normal )
-        voxel.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25 )
-
-        scene.add(voxel)
-
-        objects.append(voxel)
-
-
-
-
-if __name__ == '__main__':
-        main()
+    #post processing after resize
+    post_process()
+#-----------------------------------------------------------------------
+#RUN THE MAIN PROGRAM
+if __name__=='__main__':
+    main()
